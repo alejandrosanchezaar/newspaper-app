d9e56025ce0e60208fe3e43c0d1f1a62c39dd366
Alejandro Sánchez Arcos <alejandro.sanchezaarcos@gmail.com>
2025-10-05 19:53:04 +0200

    Initial project structure.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { ArticleCreateComponent } from '@/features/article-create/article-create.component';
+import { ArticleDetailsComponent } from '@/features/article-details/article-details.component';
+import { ArticleEditComponent } from '@/features/article-edit/article-edit.component';
+import { LoginComponent } from '@/features/login/login.component';
+import { MainComponent } from '@/features/main/main.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: MainComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'articles/create', component: ArticleCreateComponent },
+    { path: 'articles/:id', component: ArticleDetailsComponent },
+    { path: 'articles/:id/edit', component: ArticleEditComponent },
+    { path: '**', redirectTo: '/home' }
+];
---
+++ src/app/features/article-edit/article-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-edit',
+  imports: [],
+  templateUrl: './article-edit.component.html',
+  styleUrl: './article-edit.component.css'
+})
+export class ArticleEditComponent {
+
+}
