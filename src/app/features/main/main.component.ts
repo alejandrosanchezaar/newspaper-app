e73556d3d4add5bf13f9001b097c198e62ac467b
Alejandro Sánchez Arcos <alejandro.sanchezaarcos@gmail.com>
2025-10-08 22:58:42 +0200

    More improvements with the buttons, toast and filters.

--- src/app/features/main/main.component.ts
+++ src/app/features/main/main.component.ts
@@ -1,32 +1,126 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { NewsService } from '@/core/services/news.service';
+import { Article } from '@/core/models/article';
+import * as _ from 'lodash';
+import { CategoryFilterPipe } from '@/shared/pipes/category-filter.pipe';
+import { TextFilterPipe } from '@/shared/pipes/text-filter.pipe';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, CategoryFilterPipe, TextFilterPipe, RouterLink],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-cards = [
-    { title: 'Card 1', text: 'Contenido 1' },
-    { title: 'Card 2', text: 'Contenido 2' },
-    { title: 'Card 3', text: 'Contenido 3' },
-    { title: 'Card 4', text: 'Contenido 4' },
-    { title: 'Card 5', text: 'Contenido 5' },
-    { title: 'Card 6', text: 'Contenido 6' }
-  ];
+  articleList = signal<Article[]>([]);
+  selectedArticle!: Article;
+  loading: boolean = true;
+  categories = ['All', 'National', 'Economy', 'Sports', 'International', 'Technology'];
+  selectedCategories: string[] = ['All'];
+  deletingLoading: boolean = false;
+
+
+  searchText: string = '';
+  toastType: 'success' | 'error' = 'success';
+  showToast = false;
+  toastMessage: string = '';
+
+  
+  constructor(private newsService: NewsService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.getArticles();
+    this.selectedArticle = {
+    id: 0,
+    id_user: 0,
+    abstract: '',
+    subtitle: '',
+    update_date: '',
+    category: '',
+    title: '',
+    thumbnail_image: '',
+    thumbnail_media_type: '',
+    image_data: '',
+    image_media_type: ''
+};
+    
+  }
 
   // component.ts
-categories = ['Technology', 'Sports', 'Health', 'Science'];
-selectedCategories: string[] = [];
+  
 
-toggleCategory(cat: string) {
-  const index = this.selectedCategories.indexOf(cat);
-  if (index > -1) {
-    this.selectedCategories.splice(index, 1);
-  } else {
-    this.selectedCategories.push(cat);
+  private getArticles(): void {
+    this.loading = true;
+    this.newsService.getArticles().subscribe({
+      next: (articles) => {this.articleList.set(articles); console.log(articles); this.loading = false;},
+      error: (err) => {
+        console.error('Error fetching articles:', err);
+        this.toastMessage = 'Error fetching articles. Please try again later.';
+        this.showToast = true;
+        this.loading = false;
+      }
+    });
   }
-}
+
+  toggleCategory(cat: string): void {
+    if (cat === 'All') {
+      this.selectedCategories = this.selectedCategories.includes('All') ? [] : ['All'];
+      return;
+    }
+
+    const isSelected = this.selectedCategories.includes(cat);
+    this.selectedCategories = isSelected
+      ? this.selectedCategories.filter(c => c !== cat)
+      : [...this.selectedCategories.filter(c => c !== 'All'), cat];
+  }
+
+  protected goToCreateArticle(): void {
+    this.router.navigate(['/articles/create']);
+  }
+
+  protected goToEditArticle(articleID : number): void {
+    this.router.navigate(['/articles', articleID, 'edit']);
+  }
+
+  protected deleteArticle(article: Article | number): void {
+    this.deletingLoading = true;
+    // this.newsService.deleteArticle(article).subscribe({
+    //   next: (deletedArticle) => {
+    //     console.log('Artículo eliminado:', deletedArticle);
+    //     // Opcional: actualizar la lista de artículos en la UI
+    //     const id = typeof article === 'number' ? article : article.id;
+    //     this.articleList.update(list => list.filter(a => a.id !== id));
+    //   },
+    //   error: (err) => {
+    //     console.error('Error eliminando el artículo:', err);
+    //   },
+    //   complete: () => {
+    //     this.deletingLoading = false;
+    //   }
+    // });
+    setTimeout(() => { // incrementa contador en lugar de borrar
+      this.deletingLoading = false;
+      //this.displayToast('success', 'Article deleted successfully.');
+      this.displayToast('error', 'Failed to delete article.');
+
+    }, 2000);
+  }
+
+  protected showDeleteConfirmation(article: Article): void {
+    this.selectedArticle = article;
+  }
+
+
+displayToast(type: 'success' | 'error', message: string, duration: number = 3000) {
+    this.toastType = type;
+    this.toastMessage = message;
+    this.showToast = true;
+    setTimeout(() => {
+      this.showToast = false;
+    }, duration);
+  }
 }
