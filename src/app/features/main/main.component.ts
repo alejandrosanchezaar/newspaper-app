43b6aa2d10a101b3f61c4e5899518707326a0455
Alejandro Sánchez Arcos <alejandro.sanchezaarcos@gmail.com>
2025-10-09 10:02:11 +0200

    Added preliminary version of main page.

--- src/app/features/main/main.component.ts
+++ src/app/features/main/main.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { NewsService } from '@/core/services/news.service';
-import { Article } from '@/core/models/article';
+import { Article, createEmptyArticle } from '@/core/models/article';
 import * as _ from 'lodash';
 import { CategoryFilterPipe } from '@/shared/pipes/category-filter.pipe';
 import { TextFilterPipe } from '@/shared/pipes/text-filter.pipe';
@@ -22,41 +22,23 @@ export class MainComponent {
   categories = ['All', 'National', 'Economy', 'Sports', 'International', 'Technology'];
   selectedCategories: string[] = ['All'];
   deletingLoading: boolean = false;
-
-
   searchText: string = '';
   toastType: 'success' | 'error' = 'success';
   showToast = false;
   toastMessage: string = '';
 
-  
-  constructor(private newsService: NewsService, private router: Router) {}
+
+  constructor(private newsService: NewsService, private router: Router) { }
 
   ngOnInit(): void {
     this.getArticles();
-    this.selectedArticle = {
-    id: 0,
-    id_user: 0,
-    abstract: '',
-    subtitle: '',
-    update_date: '',
-    category: '',
-    title: '',
-    thumbnail_image: '',
-    thumbnail_media_type: '',
-    image_data: '',
-    image_media_type: ''
-};
-    
+    this.selectedArticle = createEmptyArticle();
   }
-
-  // component.ts
-  
 
   private getArticles(): void {
     this.loading = true;
     this.newsService.getArticles().subscribe({
-      next: (articles) => {this.articleList.set(articles); console.log(articles); this.loading = false;},
+      next: (articles) => { this.articleList.set(articles); console.log(articles); this.loading = false; },
       error: (err) => {
         console.error('Error fetching articles:', err);
         this.toastMessage = 'Error fetching articles. Please try again later.';
@@ -66,7 +48,7 @@ export class MainComponent {
     });
   }
 
-  toggleCategory(cat: string): void {
+  protected toggleCategory(cat: string): void {
     if (cat === 'All') {
       this.selectedCategories = this.selectedCategories.includes('All') ? [] : ['All'];
       return;
@@ -82,32 +64,29 @@ export class MainComponent {
     this.router.navigate(['/articles/create']);
   }
 
-  protected goToEditArticle(articleID : number): void {
+  protected goToEditArticle(articleID: number): void {
     this.router.navigate(['/articles', articleID, 'edit']);
   }
 
   protected deleteArticle(article: Article | number): void {
     this.deletingLoading = true;
-    // this.newsService.deleteArticle(article).subscribe({
-    //   next: (deletedArticle) => {
-    //     console.log('Artículo eliminado:', deletedArticle);
-    //     // Opcional: actualizar la lista de artículos en la UI
-    //     const id = typeof article === 'number' ? article : article.id;
-    //     this.articleList.update(list => list.filter(a => a.id !== id));
-    //   },
-    //   error: (err) => {
-    //     console.error('Error eliminando el artículo:', err);
-    //   },
-    //   complete: () => {
-    //     this.deletingLoading = false;
-    //   }
-    // });
-    setTimeout(() => { // incrementa contador en lugar de borrar
-      this.deletingLoading = false;
-      //this.displayToast('success', 'Article deleted successfully.');
-      this.displayToast('error', 'Failed to delete article.');
-
-    }, 2000);
+    this.newsService.deleteArticle(article).subscribe({
+      next: (deletedArticle) => {
+        console.log('Artículo eliminado:', deletedArticle);
+        const id = typeof article === 'number' ? article : article.id;
+        this.articleList.update(list => list.filter(a => a.id !== id));
+        this.selectedArticle = createEmptyArticle();
+      },
+      error: (err) => {
+        this.deletingLoading = false;
+        this.displayToast('error', 'Failed to delete article.');
+        console.error('Error:', err);
+      },
+      complete: () => {
+        this.deletingLoading = false;
+        this.displayToast('success', 'Article deleted successfully.');
+      }
+    });
   }
 
   protected showDeleteConfirmation(article: Article): void {
@@ -115,7 +94,7 @@ export class MainComponent {
   }
 
 
-displayToast(type: 'success' | 'error', message: string, duration: number = 3000) {
+  private displayToast(type: 'success' | 'error', message: string, duration: number = 3000) {
     this.toastType = type;
     this.toastMessage = message;
     this.showToast = true;
